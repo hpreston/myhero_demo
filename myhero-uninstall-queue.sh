49dd3ad72dd726a0b2c555fb38d6dc96a1081e6b
hpreston <hank.preston@gmail.com>
2016-05-17 10:47:03 -0400

    Fixing Uninstall script for Queue deployment

--- myhero-uninstall-queue.sh
+++ myhero-uninstall-queue.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 
 echo Removing Web Service
-curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/mq/web \
+curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/web \
 -H "Content-type: application/json"
 echo
 
 echo Removing App Service
-curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/mq/app \
+curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/app \
 -H "Content-type: application/json"
 echo
 
 echo Removing Ernst Service
-curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/mq/ernst \
+curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/ernst \
 -H "Content-type: application/json"
 echo
 
 echo Removing Mosca
-curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/mq/mosca \
+curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/mosca \
 -H "Content-type: application/json"
 echo
 
 
 echo Removing Data Service
-curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/mq/data \
+curl -k -X DELETE -u $MANTL_USER:$MANTL_PASSWORD https://$MANTL_CONTROL:8080/v2/apps/myhero/data \
 -H "Content-type: application/json"
 echo
 
