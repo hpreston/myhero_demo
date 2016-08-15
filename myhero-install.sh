5c451705d84733ca34014ab10547fc64d50dd654
Mike Lundy <mike@fluffypenguin.org>
2016-08-15 14:34:37 -0700

    Make install scripts work on bash 4
    In bash 4, - gets included in the variable name. Use {} to protect the
    variable name, and use set to increase the strictness of the script so
    errors aren't ignored

--- myhero-install.sh
+++ myhero-install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -euo pipefail
+
 [ -z "$MANTL_CONTROL" ] && echo "Please run 'source myhero_setup' to set Environment Variables" && exit 1;
 [ -z "$MANTL_USER" ] && echo "Please run 'source myhero_setup' to set Environment Variables" && exit 1;
 [ -z "$MANTL_PASSWORD" ] && echo "Please run 'source myhero_setup' to set Environment Variables" && exit 1;
@@ -9,33 +11,31 @@
 echo " "
 echo "***************************************************"
 echo Checking if MyHero has already been deployed with deployment name \"$DEPLOYMENT_NAME\"
-python mantl_utils.py applicationexists $DEPLOYMENT_NAME/web
-if [ $? -eq 1 ]
-then
-    echo "    Deployment name available, continuing."
-else
+if python mantl_utils.py applicationexists $DEPLOYMENT_NAME/web; then
     echo "    Deployment name already used."
     echo "    Rerun 'source myhero_setup' and choose a new deployment name."
     exit 1
+else
+    echo "    Deployment name available, continuing."
 fi
 
 # Create Copy of JSON Definitions for Deployment
 echo "Creating Service Definifition "
 
-cp sample-myhero-app.json $DEPLOYMENT_NAME-app.json
-sed -i "" -e "s/DEPLOYMENTNAME/$DEPLOYMENT_NAME/g" $DEPLOYMENT_NAME-app.json
-sed -i "" -e "s/MANTLDOMAIN/$MANTL_DOMAIN/g" $DEPLOYMENT_NAME-app.json
-sed -i "" -e "s/TAG/$TAG/g" $DEPLOYMENT_NAME-app.json
+cp sample-myhero-app.json ${DEPLOYMENT_NAME}-app.json
+sed -i "" -e "s/DEPLOYMENTNAME/${DEPLOYMENT_NAME}/g" ${DEPLOYMENT_NAME}-app.json
+sed -i "" -e "s/MANTLDOMAIN/$MANTL_DOMAIN/g" ${DEPLOYMENT_NAME}-app.json
+sed -i "" -e "s/TAG/$TAG/g" ${DEPLOYMENT_NAME}-app.json
 
 
-cp sample-myhero-data.json $DEPLOYMENT_NAME-data.json
-sed -i "" -e "s/DEPLOYMENTNAME/$DEPLOYMENT_NAME/g" $DEPLOYMENT_NAME-data.json
-sed -i "" -e "s/TAG/$TAG/g" $DEPLOYMENT_NAME-data.json
+cp sample-myhero-data.json ${DEPLOYMENT_NAME}-data.json
+sed -i "" -e "s/DEPLOYMENTNAME/${DEPLOYMENT_NAME}/g" ${DEPLOYMENT_NAME}-data.json
+sed -i "" -e "s/TAG/$TAG/g" ${DEPLOYMENT_NAME}-data.json
 
-cp sample-myhero-web.json $DEPLOYMENT_NAME-web.json
-sed -i "" -e "s/DEPLOYMENTNAME/$DEPLOYMENT_NAME/g" $DEPLOYMENT_NAME-web.json
-sed -i "" -e "s/MANTLDOMAIN/$MANTL_DOMAIN/g" $DEPLOYMENT_NAME-web.json
-sed -i "" -e "s/TAG/$TAG/g" $DEPLOYMENT_NAME-web.json
+cp sample-myhero-web.json ${DEPLOYMENT_NAME}-web.json
+sed -i "" -e "s/DEPLOYMENTNAME/${DEPLOYMENT_NAME}/g" ${DEPLOYMENT_NAME}-web.json
+sed -i "" -e "s/MANTLDOMAIN/$MANTL_DOMAIN/g" ${DEPLOYMENT_NAME}-web.json
+sed -i "" -e "s/TAG/$TAG/g" ${DEPLOYMENT_NAME}-web.json
 
 
 echo " "
@@ -44,7 +44,7 @@ echo Deploying Data Service
 echo "** Marathon Application Definition ** "
 curl -k -X POST -u $MANTL_USER:$MANTL_PASSWORD $MARATHON_URL/v2/apps \
 -H "Content-type: application/json" \
--d @$DEPLOYMENT_NAME-data.json \
+-d @${DEPLOYMENT_NAME}-data.json \
 | python -m json.tool
 echo "***************************************************"
 echo
@@ -55,7 +55,7 @@ echo Deploying Application Service
 echo "** Marathon Application Definition ** "
 curl -k -X POST -u $MANTL_USER:$MANTL_PASSWORD $MARATHON_URL/v2/apps \
 -H "Content-type: application/json" \
--d @$DEPLOYMENT_NAME-app.json \
+-d @${DEPLOYMENT_NAME}-app.json \
 | python -m json.tool
 echo
 echo "***************************************************"
@@ -66,7 +66,7 @@ sleep 7
 echo Deploying Web Service
 curl -k -X POST -u $MANTL_USER:$MANTL_PASSWORD $MARATHON_URL/v2/apps \
 -H "Content-type: application/json" \
--d @$DEPLOYMENT_NAME-web.json \
+-d @${DEPLOYMENT_NAME}-web.json \
 | python -m json.tool
 echo
 echo "***************************************************"
@@ -77,7 +77,7 @@ echo " "
 echo "Wait 5-10 minutes for the service to deploy "
 echo "and then open the following page in your browser to view the application."
 echo " "
-echo "    http://$DEPLOYMENT_NAME-web.$MANTL_DOMAIN "
+echo "    http://${DEPLOYMENT_NAME}-web.$MANTL_DOMAIN "
 echo " "
 echo " "
 
